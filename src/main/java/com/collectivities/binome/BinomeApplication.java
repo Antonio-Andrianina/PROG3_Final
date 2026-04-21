0ca4f87f89fa3c450b142a638257ad197667cbdc
Antonio-Andrianina <niokatoshimoto@gmail.com>
2026-04-21 14:03:05 +0300

    initiate base code

---
+++ src/main/java/com/collectivities/binome/BinomeApplication.java
@@ -0,0 +1,13 @@
+package com.collectivities.binome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collectivities/binome/BinomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collectivities.binome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
